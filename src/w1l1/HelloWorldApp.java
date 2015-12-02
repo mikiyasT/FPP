498048bcb413cbbdf8bada8a335a31719fb1cb86
mikiyasT <write2miki@gmail.com>
2015-12-01 18:10:50 -0600

    helloWorld modification

--- src/w1l1/HelloWorldApp.java
+++ src/w1l1/HelloWorldApp.java
@@ -9,7 +9,7 @@ public class HelloWorldApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello WOrld");
+		System.out.println("Hello WOrlddddddddddddddddddd");
 		System.out.println('\u0041');
 		
 		String str1 = "Hello";
